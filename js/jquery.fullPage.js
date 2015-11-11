5120c27b867bbef1305a3e19733fb60181d01515
Mark Steinbuch <mark@injozi.biz>
2015-11-11 12:52:45 +0200

    cleaned code
    add best

--- js/jquery.fullPage.js
+++ js/jquery.fullPage.js
@@ -56,7 +56,7 @@
     var SHOW_ACTIVE_TOOLTIP =   'fp-show-active';
 
     // slide
-    var SLIDE_DEFAULT_SEL =     '.slide';
+    var SLIDE_DEFAULT_SEL =     '.fpslide';
     var SLIDE =                 'fp-slide';
     var SLIDE_SEL =             '.' + SLIDE;
     var SLIDE_ACTIVE_SEL =      SLIDE_SEL + ACTIVE_SEL;
